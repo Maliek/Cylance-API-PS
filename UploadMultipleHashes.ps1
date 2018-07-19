e4ab332db86cb8067a1bd0aeb564e996096e1cbf
Maliek <maliek.meersschaert@hotmail.com>
2018-07-19 09:32:05 +0200

    upload multiple hashes from text file to global list

---
+++ UploadMultipleHashes.ps1
@@ -0,0 +1,13 @@
+﻿#Calculate hashes of all files in current directory
+
+dir .\ -Recurse | Get-FileHash -Algorithm SHA256 | select -ExpandProperty hash | Set-Content .\hashes.txt
+
+
+$hashes = Get-Content -Path ".\hashes.txt"
+
+
+foreach ($hash in $hashes){
+
+    Add-CyHashToGlobalList -List GlobalQuarantineList -Category None -Reason AEGIS -SHA256 $hash
+
+}
