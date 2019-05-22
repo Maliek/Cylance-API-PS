c48f53c33ffbd3f1b8483cfe1430532d1ebab92e
Maliek <maliek.meersschaert@hotmail.com>
2019-05-22 14:46:45 +0200

    adding optics delete detection by per hostname

---
+++ RemoveOpticsEventsByHost.ps1
@@ -0,0 +1,20 @@
+# Created by Maliek Meersschaert
+# Use with caution! DEMICK00701
+
+param(
+    [parameter(Mandatory=$true, ParameterSetName="Direct")]
+    [string] $hostname
+)
+
+
+Import-Module CyCLI
+
+$detections = Get-CyDetectionList | where Device -like "*$hostname*"
+
+Write-Output "Removing all detections for hostname: $hostname"
+
+Foreach ($detectionId in $detections.Id)
+{
+    Write-Output "Removing $detectionId for $hostname"
+    Remove-CyDetection -Detection $detectionId
+}
